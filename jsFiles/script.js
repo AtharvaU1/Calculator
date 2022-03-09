91847c9d8eade5a0d2159ffeab01b13826ef3364
Atharva <atharvautekar99@gmail.com>
2022-03-09 13:11:11 +0530

    added basic html and css

--- jsFiles/script.js
+++ jsFiles/script.js
@@ -13,3 +13,12 @@ function multiply(firstNum, secondNum){
 function divide(firstNum, secondNum){
     return firstNum/secondNum;
 }
+
+function operate(firstNum, secondNum, operator){
+    const ans = operator === '+' ? add(firstNum, secondNum) :
+                operator === '-' ? subtract(firstNum, secondNum) :
+                operator === '*' ? multiply(firstNum, secondNum) :
+                operator === '/' ? divide(firstNum, secondNum) : 'invalid operator'; 
+
+    return ans;
+}
