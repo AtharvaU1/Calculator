737c16b33ccc888d969093e59298db136bcd72ef
Atharva <atharvautekar99@gmail.com>
2022-03-10 17:12:35 +0530

    added decimal and erase buttons, fixed edge cases

--- jsFiles/script.js
+++ jsFiles/script.js
@@ -11,6 +11,7 @@ function multiply(firstNum, secondNum){
 }
 
 function divide(firstNum, secondNum){
+    if(secondNum == 0) return "Invalid Operation";
     return firstNum/secondNum;
 }
 
@@ -23,7 +24,7 @@ function remainder(firstNum, secondNum){
 }
 
 function operate(firstNum, secondNum, operator){
-    console.log(firstNum, secondNum, operator);       //
+    console.log(firstNum, secondNum, operator);
     const ans = operator === '+' ? add(firstNum, secondNum) :
                 operator === '-' ? subtract(firstNum, secondNum) :
                 operator === 'X' ? multiply(firstNum, secondNum) :
@@ -35,102 +36,110 @@ function operate(firstNum, secondNum, operator){
 }
 
 const buttonNodelist = document.querySelectorAll('button');
-
 buttonNodelist.forEach(button => button.addEventListener('click', display));
-//let firstNum, secondNum, operator, secondNumStart, clickedAfterResult;
-let clickedAfterResult;
+
+let clickedAfterResult, isDecimal = false;
 const operatorsArray = ['add', 'subtract', 'multiply', 'divide', 'power', 'remainder'];
 const actualOperators = ['+', '-', 'X', '÷', '^', '%'];
-// let operatorObject = {
-//     '+': 'add',
-//     '-': 'subtract', 
-//     'X': 'multiply', 
-//     '÷': 'divide', 
-//     '^':'power', 
-//     '%': 'remainder',
-// };
 
 function clear(){
-    // firstNum = undefined;
-    // secondNum = undefined;
-    // operator = '';
-    // secondNumStart = undefined;
     document.querySelector('.display').textContent = '';
     clickedAfterResult = false;
+    enableDecimal();
 }
 
 function isTargetClassOperator(str){
     return (actualOperators.includes(str) || operatorsArray.includes(str));
 }
 
+function roundToTwo(num) {    
+    return +(Math.round(num + "e+2")  + "e-2");
+}
+
 function evaluateExpression(expression){
     console.log(expression);
 
-    let ans = 0;
-    let numOne, numTwo, expLen = expression.length, operator, secondStart;
-
-    if(isTargetClassOperator(expression[0]) || isTargetClassOperator(expression[expLen-1])){
+    if(isTargetClassOperator(expression[0]) || isTargetClassOperator(expression[expression.length-1])){
         clear();
-        return "Invalid Input."
+        return "Invalid Input";
     }
+    if(expression[0]==='.'){
+        expression = '0' + expression;
+    }
+    if(expression[expression.length-1]==='.'){
+        expression += '0';
+    }
+
+    let numOne, numTwo, expLen = expression.length, operator, secondStart;
 
     for(let i = 0 ; i < expLen ; i++){
         if(isTargetClassOperator(expression[i])){
             if(numOne==undefined){
-                numOne = parseInt(expression.substring(0, i));
+                numOne = parseFloat(expression.substring(0, i));
                 secondStart = i+1;
             }
             else{
-                numTwo = parseInt(expression.substring(secondStart, i));
+                numTwo = parseFloat(expression.substring(secondStart, i));
                 console.log(numTwo, operator);
                 numOne = operate(numOne, numTwo, operator);
+                if(isNaN(numOne)) return numOne;
                 secondStart = i+1;
             }
             operator = expression[i];
             console.log(numOne, numTwo, operator);
         }
     }
-    console.log(numOne, numTwo, operator);          //
-    return operate(numOne, parseInt(expression.substring(secondStart, expLen)), operator);
+    console.log(numOne, numTwo, operator);          
+    const ans = operate(numOne, parseFloat(expression.substring(secondStart, expLen)), operator);
+    const finalAns = roundToTwo(ans);
+    return (isNaN(finalAns)) ? "Invalid Operation" : finalAns;  
+}
 
-    //return ans;
+function enableDecimal(){
+    document.querySelector('.decimal').disabled = false;
+    isDecimal = false;
+}
+function disableDecimal(){
+    document.querySelector('.decimal').disabled = true;
+    isDecimal = true;
 }
 
 function display(e){
     const text = e.target.innerText;
     const targetClass = e.target.className;
-    if(targetClass==='clear' || targetClass==='erase'){ // yet to implement erase()
+    const displayDiv = document.querySelector('.display');
+    const displayDivContent = displayDiv.textContent;
+
+    if(targetClass==='clear' || targetClass==='erase'){
         if(targetClass==='clear'){
             clear();
         }
+        else if(targetClass==='erase'){
+            const len = displayDivContent.length;
+            if(displayDivContent[len-1]==='.') isDecimal = false;
+            if(len!==0) displayDiv.textContent = displayDivContent.slice(0, len-1);
+        }
         return;
-    } 
+    }
 
-    const displayDiv = document.querySelector('.display');
+    if(targetClass==='decimal' && !isDecimal){
+        disableDecimal();               
+    }
+    if(isTargetClassOperator(targetClass) && isDecimal){
+        enableDecimal();     
+    }
+
     if(clickedAfterResult === true && !isTargetClassOperator(targetClass)){
         clickedAfterResult = false;
         displayDiv.textContent = '';
         clear();
-    }else{
-        clickedAfterResult = false;
-    }
+    }else clickedAfterResult = false;
+
     if(targetClass==='equals'){
-        console.log(displayDiv.textContent);        //
+        console.log(displayDiv.textContent);
         displayDiv.textContent = evaluateExpression(displayDiv.textContent);
         clickedAfterResult = true;
+        enableDecimal();
     }
     if(targetClass!=='equals') displayDiv.textContent += text;
 }
-
-// if(isTargetClassOperator(targetClass)){
-    //     if(firstNum==undefined) firstNum = parseInt(displayDiv.textContent);
-    //     operator = text;
-    //     secondNumStart = displayDiv.textContent.length+1;
-    // }else if(targetClass==='equals'){
-    //     const secondString = displayDiv.textContent.substring(secondNumStart, displayDiv.textContent.length);
-    //     secondNum = parseInt(secondString);
-    //     const ans = operate(firstNum, secondNum, operator);
-    //     firstNum = ans;
-    //     displayDiv.textContent = ans;
-    //     clickedAfterResult = true;
-    // }
